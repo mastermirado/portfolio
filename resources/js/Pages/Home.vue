56540449d4f7f30225e7768f92394ce51f66e58e
Mirado RAKOTOARISON <mirado.rakotoarison@centraltest.com>
2025-03-25 14:55:20 +0100

    add homepage

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    Welcome to my portfolio v1.0
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,11 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
+Route::get('/', function(){
+    return Inertia::render('Home');
+});
+
+Route::get('/welcome', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
